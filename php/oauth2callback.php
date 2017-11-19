fa46270ab8f0fc350db84df59dedc8fb12db4924
Keith Moss <keithamoss@gmail.com>
2017-11-19 11:12:59 +0800

    Auth now refreshes access token as needs be

--- php/oauth2callback.php
+++ php/oauth2callback.php
@@ -1,5 +1,6 @@
 <?php
-require_once 'google-api-php-client/src/Google/autoload.php';
+// require_once 'google-api-php-client/src/Google/autoload.php';
+require_once 'google-api-php-client-2.2.0/vendor/autoload.php';
 
 if(strpos($_SERVER['HTTP_HOST'], "localhost:") === 0) {
   session_set_cookie_params(86400 * 365 * 5, "/");
@@ -10,7 +11,7 @@ session_start();
 
 $client = new Google_Client();
 $client->setAccessType('offline');
-// $client->setApprovalPrompt('force');
+$client->setApprovalPrompt('force');
 $client->setAuthConfigFile('client_secrets.inc');
 $client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php');
 $client->addScope(Google_Service_Plus::USERINFO_EMAIL);
@@ -19,14 +20,10 @@ if (! isset($_GET['code'])) {
   $auth_url = $client->createAuthUrl();
   header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
 } else {
-  $client->authenticate($_GET['code']);
+  $resp = $client->authenticate($_GET['code']);
   $_SESSION['access_token'] = $client->getAccessToken();
-
-  // echo 'OK!<br>';
-  // echo '<pre>';
-  // print_r(json_decode($_SESSION['access_token']));
-  // echo '</pre>';
-  // exit;
+  // store and use $refreshToken to get new access tokens
+  $_SESSION['refresh_token'] = $resp['refresh_token'];
 
   $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/login.php';
   header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
--- php/self.php
+++ php/self.php
@@ -3,7 +3,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once 'google-api-php-client/src/Google/autoload.php';
+// require_once 'google-api-php-client/src/Google/autoload.php';
+require_once 'google-api-php-client-2.2.0/vendor/autoload.php';
 
 if(strpos($_SERVER['HTTP_HOST'], "localhost:") === 0) {
   session_set_cookie_params(86400 * 365 * 5, "/");
@@ -27,6 +28,25 @@ $client->addScope(Google_Service_Plus::USERINFO_EMAIL);
 header("Content-type: application/json");
 if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
   $client->setAccessToken($_SESSION['access_token']);
+
+  // if($client->isAccessTokenExpired()) {
+  //   // $authUrl = $client->createAuthUrl();
+  //   // header('Location: ' . filter_var($authUrl, FILTER_SANITIZE_URL));
+
+  //   echo "Expired!<br>!";
+  //   echo $_SESSION["refresh_token"].'<br>';
+  //   echo $_SESSION['access_token'].'<br>';
+  //   $client->refreshToken($_SESSION["refresh_token"]);
+
+  //   // $client->authenticate($_SESSION['access_token']);
+  //   // $NewAccessToken = json_decode($client->getAccessToken());
+  //   // $client->refreshToken($NewAccessToken->refresh_token);
+  //   echo 1;
+  //   exit;
+  // }
+  // echo 1;
+  // exit;
+
   $plus_service = new Google_Service_Plus($client);
   $me = $plus_service->people->get("me", array("fields" => "emails"));
 
