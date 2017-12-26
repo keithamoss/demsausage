6437d4a80f7dcc3066d7a7f2f028b1dff52bfeef
Keith Moss <keithamoss@gmail.com>
2017-12-26 17:30:06 +0800

    PollingPlaceTypes editor now has a sort-of empty state

--- admin/src/polling_places/polling_place_types_editor/PollingPlaceTypesEditorContainer.tsx
+++ admin/src/polling_places/polling_place_types_editor/PollingPlaceTypesEditorContainer.tsx
@@ -1,9 +1,36 @@
 import * as React from "react"
 import { connect } from "react-redux"
+import styled from "styled-components"
 
 import PollingPlaceTypesEditor from "./PollingPlaceTypesEditor"
 import { IStore, IElection, IPollingPlace } from "../../redux/modules/interfaces"
 import { fetchAllPollingPlaces, updatePollingPlace } from "../../redux/modules/polling_places"
+
+import { GridTile } from "material-ui/GridList"
+import CommunicationLocationOff from "material-ui/svg-icons/communication/location-off"
+
+const EmptyState = styled.div`
+  max-width: 250px;
+  max-height: 250px;
+  opacity: 0.4;
+  margin: auto;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+`
+
+const CenteredCommunicationLocationOff = styled(CommunicationLocationOff)`
+  width: 125px !important;
+  height: 125px !important;
+  margin: auto;
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+`
 
 export interface IStoreProps {
   election: IElection
@@ -35,6 +62,24 @@ export class PollingPlaceTypesEditorContainer extends React.PureComponent<IStore
   render() {
     const { pollingPlaces, pollingPlaceTypes, election, updatePollingPlaceType } = this.props
 
+    if (election.db_table_name === "") {
+      return (
+        <EmptyState>
+          <GridTile
+            title={
+              <div>
+                We don't have any polling<br />places for this election yet :(
+              </div>
+            }
+            titleBackground={"rgb(255, 255, 255)"}
+            titleStyle={{ color: "black" }}
+          >
+            <CenteredCommunicationLocationOff />
+          </GridTile>
+        </EmptyState>
+      )
+    }
+
     return (
       <PollingPlaceTypesEditor
         pollingPlaces={pollingPlaces}
@@ -60,7 +105,9 @@ const mapStateToProps = (state: IStore, ownProps: IOwnProps): IStoreProps => {
 const mapDispatchToProps = (dispatch: Function): IDispatchProps => {
   return {
     fetchInitialState: (election: IElection) => {
-      dispatch(fetchAllPollingPlaces(election))
+      if (election.db_table_name !== "") {
+        dispatch(fetchAllPollingPlaces(election))
+      }
     },
     updatePollingPlaceType: (election: IElection, pollingPlace: IPollingPlace, newType: string) => {
       dispatch(updatePollingPlace(election, pollingPlace, { polling_place_type: newType }))
