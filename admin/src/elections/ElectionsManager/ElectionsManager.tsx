946858b837984db59ac5a7914fa92dbe4d206501
Keith Moss <keithamoss@gmail.com>
2018-02-14 18:16:24 +0800

    Shush, linter

--- admin/src/elections/ElectionsManager/ElectionsManager.tsx
+++ admin/src/elections/ElectionsManager/ElectionsManager.tsx
@@ -10,58 +10,59 @@ import IconButton from "material-ui/IconButton"
 import FileFileUpload from "material-ui/svg-icons/file/file-upload"
 
 export interface IProps {
-  elections: Array<IElection>
+    elections: Array<IElection>
 }
 
 class ElectionsManager extends React.PureComponent<IProps, {}> {
-  onClickElection: Function
-  onClickFileUpload: Function
+    onClickElection: Function
+    onClickFileUpload: Function
 
-  constructor(props: any) {
-    super(props)
+    constructor(props: any) {
+        super(props)
 
-    this.onClickElection = (election: any) => {
-      browserHistory.push(`/election/${election.id}/`)
+        this.onClickElection = (election: any) => {
+            browserHistory.push(`/election/${election.id}/`)
+        }
+        this.onClickFileUpload = (election: any) => {
+            browserHistory.push(`/election/${election.id}/load_polling_places/`)
+        }
     }
-    this.onClickFileUpload = (election: any) => {
-      browserHistory.push(`/election/${election.id}/load_polling_places/`)
-    }
-  }
-  render() {
-    const { elections } = this.props
+    render() {
+        const { elections } = this.props
 
-    return (
-      <div>
-        <RaisedButton label={"Create Election"} primary={true} containerElement={<Link to={`/election/new`} />} />
-        <List>
-          {elections.map((election: IElection) => (
-            <ListItem
-              key={election.id}
-              primaryText={election.name}
-              secondaryText={
-                <span>
-                  {new Date(election.election_day).toLocaleDateString("en-AU", {
-                    weekday: "long",
-                    day: "2-digit",
-                    month: "long",
-                    year: "numeric",
-                  })}
-                  {election.is_active ? " (ACTIVE)" : undefined}
-                  {election.hidden ? " (HIDDEN)" : undefined}
-                </span>
-              }
-              onClick={this.onClickElection.bind(this, election)}
-              rightIconButton={
-                <IconButton tooltip="Load polling places" onClick={this.onClickFileUpload.bind(this, election)}>
-                  <FileFileUpload />
-                </IconButton>
-              }
-            />
-          ))}
-        </List>
-      </div>
-    )
-  }
+        return (
+            <div>
+                <RaisedButton label={"Create Election"} primary={true} containerElement={<Link to={`/election/new`} />} />
+                <List>
+                    {elections.map((election: IElection) => (
+                        <ListItem
+                            key={election.id}
+                            primaryText={election.name}
+                            secondaryText={
+                                <span>
+                                    {new Date(election.election_day).toLocaleDateString("en-AU", {
+                                        weekday: "long",
+                                        day: "2-digit",
+                                        month: "long",
+                                        year: "numeric",
+                                    })}
+                                    {election.is_active ? " (ACTIVE)" : undefined}
+                                    {election.hidden ? " (HIDDEN)" : undefined}
+                                </span>
+                            }
+                            onClick={this.onClickElection.bind(this, election)}
+                            rightIconButton={
+                                <IconButton tooltip="Load polling places" onClick={this.onClickFileUpload.bind(this, election)}>
+                                    <FileFileUpload />
+                                </IconButton>
+                                // tslint:disable-next-line:jsx-curly-spacing
+                            }
+                        />
+                    ))}
+                </List>
+            </div>
+        )
+    }
 }
 
 export default ElectionsManager
--- admin/src/polling_places/polling_place_types_editor/PollingPlaceTypesEditor.tsx
+++ admin/src/polling_places/polling_place_types_editor/PollingPlaceTypesEditor.tsx
@@ -11,92 +11,97 @@ import ActionOpenInNew from "material-ui/svg-icons/action/open-in-new"
 import { blueGrey50 } from "material-ui/styles/colors"
 
 const GridWrapper = styled.div`
-  width: 100%;
-  display: grid;
-  grid-gap: 2%;
-  grid-template-columns: repeat(1, 100%);
+    width: 100%;
+    display: grid;
+    grid-gap: 2%;
+    grid-template-columns: repeat(1, 100%);
 `
 
 const GridBox = styled.div`
-  width: 100%;
-  display: grid;
-  grid-gap: 1%;
-  grid-template-columns: 25% 60% 15%;
-  border-bottom: 1px solid ${blueGrey50};
+    width: 100%;
+    display: grid;
+    grid-gap: 1%;
+    grid-template-columns: 25% 60% 15%;
+    border-bottom: 1px solid ${blueGrey50};
 `
 
 export interface IProps {
-  pollingPlaces: Array<IPollingPlace>
-  pollingPlaceTypes: Array<string>
-  onChangeType: any
+    pollingPlaces: Array<IPollingPlace>
+    pollingPlaceTypes: Array<string>
+    onChangeType: any
 }
 
 // Work around TypeScript not allowing us to pass <a> props to react-router's <Link>
 class CustomLink extends React.Component<any, any> {
-  render(): any {
-    return <Link {...this.props} />
-  }
+    render(): any {
+        return <Link {...this.props} />
+    }
 }
 
 class PollingPlaceTypesEditor extends React.PureComponent<IProps, {}> {
-  onChangeType: Function
+    onChangeType: Function
 
-  constructor(props: any) {
-    super(props)
+    constructor(props: any) {
+        super(props)
 
-    this.onChangeType = (pollingPlace: IPollingPlace, e: any, value: string) => {
-      props.onChangeType(value, pollingPlace)
-    }
-  }
-
-  render() {
-    const { pollingPlaces, pollingPlaceTypes } = this.props
-
-    const styles = {
-      radio: { display: "inline-block", width: "33%" },
+        this.onChangeType = (pollingPlace: IPollingPlace, e: any, value: string) => {
+            props.onChangeType(value, pollingPlace)
+        }
     }
 
-    const pollingPlacesSansType = pollingPlaces.filter((pollingPlace: IPollingPlace) => pollingPlace.polling_place_type === "")
+    render() {
+        const { pollingPlaces, pollingPlaceTypes } = this.props
 
-    return (
-      <GridWrapper>
-        <VirtualList
-          width="100%"
-          height={900}
-          itemCount={pollingPlacesSansType.length}
-          itemSize={122.25}
-          renderItem={({ index, style }: any) => (
-            <div key={index} style={style}>
-              <GridBox>
-                <div>
-                  <h3>{pollingPlacesSansType[index].polling_place_name}</h3>
-                  <h4>{pollingPlacesSansType[index].premises}</h4>
-                </div>
-                <div>
-                  <RadioButtonGroup
-                    name={`pollingPlaceTypes-${pollingPlacesSansType[index].id}`}
-                    onChange={this.onChangeType.bind(this, pollingPlacesSansType[index])}
-                  >
-                    {pollingPlaceTypes.map((type: string) => <RadioButton key={type} value={type} label={type} style={styles.radio} />)}
-                  </RadioButtonGroup>
-                </div>
-                <div>
-                  <RaisedButton
-                    label="Google It"
-                    primary={true}
-                    icon={<ActionOpenInNew />}
-                    containerElement={
-                      <CustomLink to={`https://www.google.com.au/search?q=${pollingPlacesSansType[index].premises}`} target="_blank" />
-                    }
-                  />
-                </div>
-              </GridBox>
-            </div>
-          )}
-        />
-      </GridWrapper>
-    )
-  }
+        const styles = {
+            radio: { display: "inline-block", width: "33%" },
+        }
+
+        const pollingPlacesSansType = pollingPlaces.filter((pollingPlace: IPollingPlace) => pollingPlace.polling_place_type === "")
+
+        return (
+            <GridWrapper>
+                <VirtualList
+                    width="100%"
+                    height={900}
+                    itemCount={pollingPlacesSansType.length}
+                    itemSize={122.25}
+                    renderItem={({ index, style }: any) => (
+                        <div key={index} style={style}>
+                            <GridBox>
+                                <div>
+                                    <h3>{pollingPlacesSansType[index].polling_place_name}</h3>
+                                    <h4>{pollingPlacesSansType[index].premises}</h4>
+                                </div>
+                                <div>
+                                    <RadioButtonGroup
+                                        name={`pollingPlaceTypes-${pollingPlacesSansType[index].id}`}
+                                        onChange={this.onChangeType.bind(this, pollingPlacesSansType[index])}
+                                    >
+                                        {pollingPlaceTypes.map((type: string) => (
+                                            <RadioButton key={type} value={type} label={type} style={styles.radio} />
+                                        ))}
+                                    </RadioButtonGroup>
+                                </div>
+                                <div>
+                                    <RaisedButton
+                                        label="Google It"
+                                        primary={true}
+                                        icon={<ActionOpenInNew />}
+                                        containerElement={
+                                            <CustomLink
+                                                to={`https://www.google.com.au/search?q=${pollingPlacesSansType[index].premises}`}
+                                                target="_blank"
+                                            />
+                                        }
+                                    />
+                                </div>
+                            </GridBox>
+                        </div>
+                    )}
+                />
+            </GridWrapper>
+        )
+    }
 }
 
 export default PollingPlaceTypesEditor
