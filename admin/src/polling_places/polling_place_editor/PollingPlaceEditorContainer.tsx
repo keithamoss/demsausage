1df5b4d9e6dd0ca6e09763e0590890919b15bf46
Keith Moss <keithamoss@gmail.com>
2018-02-18 15:29:03 +0800

    Better EmptyState logic for PollingPlaceEditor

--- admin/src/polling_places/polling_place_editor/PollingPlaceEditorContainer.tsx
+++ admin/src/polling_places/polling_place_editor/PollingPlaceEditorContainer.tsx
@@ -9,87 +9,85 @@ import { IStore, IElection, IPollingPlace } from "../../redux/modules/interfaces
 import CommunicationLocationOff from "material-ui/svg-icons/communication/location-off"
 
 export interface IProps {
-  election: IElection
-  pollingPlaceId: number | null
-  showAutoComplete: boolean
-  onPollingPlaceEdited: Function
+    election: IElection
+    pollingPlaceId: number | null
+    showAutoComplete: boolean
+    onPollingPlaceEdited: Function
 }
 export interface IStoreProps {}
 
 export interface IDispatchProps {
-  fetchRequiredState: Function
+    fetchRequiredState: Function
 }
 
 export interface IStateProps {
-  pollingPlace?: IPollingPlace
+    pollingPlacesChecked: boolean
+    pollingPlace?: IPollingPlace
 }
 
 interface IOwnProps {}
 
 export class PollingPlaceEditorContainer extends React.Component<IProps & IDispatchProps, IStateProps> {
-  async componentWillMount() {
-    const { fetchRequiredState, election, pollingPlaceId } = this.props
+    constructor(props: IProps & IDispatchProps) {
+        super(props)
 
-    if (pollingPlaceId !== null && election.db_table_name !== "") {
-      this.setState({ pollingPlace: await fetchRequiredState(election, pollingPlaceId) })
-    }
-  }
-
-  async componentWillReceiveProps(nextProps: any) {
-    const { fetchRequiredState, election, pollingPlaceId } = nextProps
-
-    if (pollingPlaceId !== null) {
-      this.setState({ pollingPlace: await fetchRequiredState(election, pollingPlaceId) })
-    } else if (pollingPlaceId === null) {
-      await this.setState({ pollingPlace: undefined })
-    }
-  }
-
-  render() {
-    const { election, showAutoComplete, onPollingPlaceEdited } = this.props
-    const pollingPlace: any = this.state !== null && this.state.pollingPlace !== null ? this.state.pollingPlace : null
-
-    if (election.db_table_name === "") {
-      return (
-        <EmptyState
-          message={
-            <div>
-              We don't have any polling<br />places for this election yet :(
-            </div>
-          }
-          icon={<CommunicationLocationOff />}
-        />
-      )
+        this.state = { pollingPlacesChecked: false }
     }
 
-    return (
-      <PollingPlaceEditor
-        election={election}
-        pollingPlace={pollingPlace}
-        showAutoComplete={showAutoComplete}
-        onPollingPlaceEdited={onPollingPlaceEdited}
-      />
-    )
-  }
+    async componentWillMount() {
+        const { fetchRequiredState, election, pollingPlaceId } = this.props
+
+        if (pollingPlaceId !== null && election.db_table_name !== "") {
+            this.setState({ pollingPlacesChecked: true, pollingPlace: await fetchRequiredState(election, pollingPlaceId) })
+        }
+    }
+
+    async componentWillReceiveProps(nextProps: any) {
+        const { fetchRequiredState, election, pollingPlaceId } = nextProps
+
+        if (pollingPlaceId !== null) {
+            this.setState({ pollingPlacesChecked: true, pollingPlace: await fetchRequiredState(election, pollingPlaceId) })
+        } else if (pollingPlaceId === null) {
+            await this.setState({ pollingPlacesChecked: true, pollingPlace: undefined })
+        }
+    }
+
+    render() {
+        const { election, showAutoComplete, onPollingPlaceEdited } = this.props
+        const pollingPlace: any = this.state !== null && this.state.pollingPlace !== null ? this.state.pollingPlace : null
+
+        if (election.db_table_name === "" || (this.state.pollingPlacesChecked && this.state.pollingPlace === null)) {
+            return <EmptyState message={<div>No polling place found.</div>} icon={<CommunicationLocationOff />} />
+        }
+
+        return (
+            <PollingPlaceEditor
+                election={election}
+                pollingPlace={pollingPlace}
+                showAutoComplete={showAutoComplete}
+                onPollingPlaceEdited={onPollingPlaceEdited}
+            />
+        )
+    }
 }
 
 const mapStateToProps = (state: IStore, ownProps: IOwnProps): IStoreProps => {
-  // const { elections } = state
+    // const { elections } = state
 
-  return {}
+    return {}
 }
 
 const mapDispatchToProps = (dispatch: Function): IDispatchProps => {
-  return {
-    fetchRequiredState: async (election: IElection, pollingPlaceId: number) => {
-      const pollingPlaces: Array<IPollingPlace> = await dispatch(fetchPollingPlacesByIds(election, [pollingPlaceId]))
-      if (pollingPlaces.length === 1) {
-        return pollingPlaces[0]
-      }
-      // @TOOD Gracefully handle no polling place being returned
-      return null
-    },
-  }
+    return {
+        fetchRequiredState: async (election: IElection, pollingPlaceId: number) => {
+            const pollingPlaces: Array<IPollingPlace> = await dispatch(fetchPollingPlacesByIds(election, [pollingPlaceId]))
+            if (pollingPlaces.length === 1) {
+                return pollingPlaces[0]
+            }
+            // @TOOD Gracefully handle no polling place being returned
+            return null
+        },
+    }
 }
 
 const PollingPlaceEditorContainerWrapped = connect(mapStateToProps, mapDispatchToProps)(PollingPlaceEditorContainer)
