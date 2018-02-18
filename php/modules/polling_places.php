2460f89710a007444a25de412b1b2ed889857bbc
Keith Moss <keithamoss@gmail.com>
2018-02-18 15:38:59 +0800

    Polling Place division is no longer a mandatory field

--- php/modules/polling_places.php
+++ php/modules/polling_places.php
@@ -1,7 +1,7 @@
 <?php
 $pollingPlacesPKeyFieldName = "id";
 $pollingPlacesAllowedFields = array("stall_description", "lon", "has_run_out", "has_nothing", "has_caek", "has_bbq", "chance_of_sausage", "latest_report", "first_report", "stall_website", "lat", "stall_name", "extra_info", "source", "has_other", "wheelchairaccess", "entrancesdesc", "polling_place_type", "booth_info", "opening_hours", "premises", "address", "polling_place_name", "division", "state", "ess_stall_id", "ess_stall_url");
-$pollingPlacesRequiredFields = array("lon", "lat", "polling_place_name", "address", "division", "state");
+$pollingPlacesRequiredFields = array("lon", "lat", "polling_place_name", "address", "state");
 $validStates = ["WA", "SA", "NT", "VIC", "NSW", "QLD", "TAS", "ACT", "Overseas"];
 $validPollingPlaceTypes = ["Child Care Centre", "Church", "Community Hall", "Other", "Pre-School", "Private School", "Public School", "Senior Citizens Centre", "Courthouse", "Hospital"];
 
