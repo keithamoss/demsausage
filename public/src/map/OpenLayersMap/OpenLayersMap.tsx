bc0e4b20bc1c68174d4625daf05ce0b0aee159e7
Keith Moss <keithamoss@gmail.com>
2018-03-13 15:35:05 +0800

    Adding a backup tile server that we can easily toggle on

--- public/src/map/OpenLayersMap/OpenLayersMap.tsx
+++ public/src/map/OpenLayersMap/OpenLayersMap.tsx
@@ -117,6 +117,13 @@ class OpenLayersMap extends React.PureComponent<IProps, {}> {
                         crossOrigin: "anonymous",
                     }),
                 }),
+                // new ol.layer.Tile({
+                //     source: new ol.source.OSM({
+                //         // https://carto.com/location-data-services/basemaps/
+                //         // url: "http://{a-c}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png",
+                //         attributions: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors.`,
+                //     }),
+                // }),
             ],
             target: "openlayers-map",
             controls: [],
