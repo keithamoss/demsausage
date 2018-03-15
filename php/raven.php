fad744f40c2bbaf91ea0a23417e56b9c23acdbf9
Keith Moss <keithamoss@gmail.com>
2018-03-15 16:36:19 +0800

    I've got a thousand issues, all of them about timezones
    Solving "Module 'timezonedb' already loaded" errors...hopefully

--- php/raven.php
+++ php/raven.php
@@ -1,6 +1,7 @@
 <?php
 ini_set("display_errors", 0);
 ini_set("display_startup_errors", 0);
+ini_set("date.timezone", "Australia/Perth");
 ini_set("error_log", "../php-error.log");
 
 require_once "../secrets.php";
