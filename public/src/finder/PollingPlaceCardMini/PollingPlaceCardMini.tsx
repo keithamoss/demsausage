9c070ba1bbee7fa550974749e7c11fbb6e6a641c
Keith Moss <keithamoss@gmail.com>
2018-11-24 09:27:01 +0800

    Stall descriptions now display across multiple lines
    Fixes #22

--- public/src/finder/PollingPlaceCardMini/PollingPlaceCardMini.tsx
+++ public/src/finder/PollingPlaceCardMini/PollingPlaceCardMini.tsx
@@ -1,30 +1,28 @@
+import Avatar from "material-ui/Avatar"
+import { Card, CardHeader, CardText, CardTitle } from "material-ui/Card"
+import FlatButton from "material-ui/FlatButton"
+import { ListItem } from "material-ui/List"
+import Paper from "material-ui/Paper"
+import { grey500, yellow600 } from "material-ui/styles/colors"
+import { ActionHelpOutline /*, MapsAddLocation*/, AlertWarning, MapsNavigation } from "material-ui/svg-icons"
 import * as React from "react"
 import styled from "styled-components"
+import BaconandEggsIcon from "../../icons/bacon-and-eggs"
+import CakeIcon from "../../icons/cake"
+import CoffeeIcon from "../../icons/coffee"
+import HalalIcon from "../../icons/halal"
+import RedCrossofShameIcon from "../../icons/red-cross-of-shame"
+import SausageIcon from "../../icons/sausage"
+import VegoIcon from "../../icons/vego"
 // import { Link, browserHistory } from "react-router"
 // import "./PollingPlaceCardMini.css"
 import { IElection } from "../../redux/modules/interfaces"
 import {
+    getFoodDescription,
+    getSausageChanceDescription,
     pollingPlaceHasReports,
     pollingPlaceHasReportsOfNoms,
-    getSausageChanceDescription,
-    getFoodDescription,
 } from "../../redux/modules/polling_places"
-
-import Paper from "material-ui/Paper"
-import { Card, CardHeader, CardTitle, CardText } from "material-ui/Card"
-import FlatButton from "material-ui/FlatButton"
-import { ListItem } from "material-ui/List"
-import Avatar from "material-ui/Avatar"
-import { MapsNavigation, AlertWarning, ActionHelpOutline /*, MapsAddLocation*/ } from "material-ui/svg-icons"
-import { grey500, yellow600 } from "material-ui/styles/colors"
-
-import SausageIcon from "../../icons/sausage"
-import CakeIcon from "../../icons/cake"
-import VegoIcon from "../../icons/vego"
-import RedCrossofShameIcon from "../../icons/red-cross-of-shame"
-import HalalIcon from "../../icons/halal"
-import CoffeeIcon from "../../icons/coffee"
-import BaconandEggsIcon from "../../icons/bacon-and-eggs"
 
 const FlexboxContainer = styled.div`
     display: flex;
@@ -100,7 +98,11 @@ class PollingPlaceCardMini extends React.PureComponent<IProps, {}> {
                     <img src="images/nature-600-337.jpg" alt="" />
                 </CardMedia> */}
                     {pollingPlace.stall_name !== null && (
-                        <CardTitle title={pollingPlace.stall_name} subtitle={pollingPlace.stall_description} />
+                        <CardTitle
+                            title={pollingPlace.stall_name}
+                            subtitle={pollingPlace.stall_description}
+                            subtitleStyle={{ whiteSpace: "pre" }}
+                        />
                     )}
                     <CardText>
                         <FlexboxContainer>
@@ -120,10 +122,9 @@ class PollingPlaceCardMini extends React.PureComponent<IProps, {}> {
                                 />
                             )}
                         </FlexboxContainer>
-                        {pollingPlace.has_other !== null &&
-                            "has_free_text" in pollingPlace.has_other && (
-                                <HasFreeTextDeliciousness>Also available: {pollingPlace.has_other.has_free_text}</HasFreeTextDeliciousness>
-                            )}
+                        {pollingPlace.has_other !== null && "has_free_text" in pollingPlace.has_other && (
+                            <HasFreeTextDeliciousness>Also available: {pollingPlace.has_other.has_free_text}</HasFreeTextDeliciousness>
+                        )}
                         {pollingPlace.has_run_out && (
                             <RunOutWarning
                                 secondaryText={"We've had reports that the stalls at this polling booth have run out of food."}
