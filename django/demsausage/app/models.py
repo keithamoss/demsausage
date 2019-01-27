7c6eb79fe16213682acae96e71d5e9bc5c7e2ad1
Keith Moss <keithamoss@gmail.com>
2019-01-27 14:30:26 +0800

    flake8

--- django/demsausage/app/models.py
+++ django/demsausage/app/models.py
@@ -1,4 +1,3 @@
-from django.db import models
 from django.contrib.postgres.fields import JSONField
 from django.contrib.auth.models import User
 from django.contrib.gis.db import models
--- django/demsausage/app/validators.py
+++ django/demsausage/app/validators.py
@@ -1,10 +1,11 @@
 from jsonschema import validate
 from django.core.validators import BaseValidator
+from django.core.exceptions import ValidationError
 
 
 class JSONSchemaValidator(BaseValidator):
     def compare(self, a, b):
         try:
             validate(a, b)
-        except:
-            raise ValidationError(_('%(value)s is not schema'), params={'value': a}, )
+        except Exception:
+            raise ValidationError("%(value)s is not schema", params={"value": a}, )
