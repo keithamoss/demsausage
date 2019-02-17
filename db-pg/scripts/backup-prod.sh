d5a62d160fd70148fdbd59756a81ad3a42895b04
Keith Moss <keithamoss@gmail.com>
2019-02-17 20:17:49 +0800

    More database management scripts

---
+++ db-pg/scripts/backup-prod.sh
@@ -0,0 +1,9 @@
+now=`date '+%Y_%m_%d__%H_%M_%S'`
+
+echo "Dump production database"
+export $(xargs < secrets/sausage-prod-db.env)
+echo "${DB_HOST}:${DB_PORT}:${DB_NAME}:${DB_USERNAME}:${DB_PASSWORD}" > ~/.pgpass
+chmod 600 ~/.pgpass
+
+prod_db_dump_file="digitalocean_db-a_${DB_SCHEMA}_${now}.dump"
+pg_dump --host=$DB_HOST --username=$DB_USERNAME --format=custom --schema=$DB_SCHEMA $DB_NAME > $prod_db_dump_file
---
+++ db-pg/scripts/restore-prod.sh
@@ -0,0 +1,7 @@
+echo "Load database dump to production"
+export $(xargs < secrets/sausage-prod-db.env)
+echo "${DB_HOST}:${DB_PORT}:${DB_NAME}:${DB_USERNAME}:${DB_PASSWORD}" > ~/.pgpass
+chmod 600 ~/.pgpass
+
+db_dump_file=local_dev_db_demsausage_2019_02_17__12_17_18.dump
+pg_restore --host=$DB_HOST --username=$DB_USERNAME --dbname=$DB_NAME --single-transaction $db_dump_file
