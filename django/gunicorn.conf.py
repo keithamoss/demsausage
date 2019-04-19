254c591ed42ef2f692d48782ae1704b10e542e85
Keith Moss <keithamoss@gmail.com>
2019-04-19 11:12:04 +0800

    Replacing UWSGI with Gunicorn

---
+++ django/gunicorn.conf.py
@@ -0,0 +1,17 @@
+"""gunicorn WSGI server configuration."""
+from multiprocessing import cpu_count
+from os import environ
+
+
+def max_workers():
+    return (2 * cpu_count()) + 1
+
+
+bind = "0.0.0.0:" + environ.get("PORT", "8000")
+max_requests = 1000
+worker_class = "gevent"
+workers = max_workers()
+forwarded_allow_ips = "*"
+loglevel = "info"
+pythonpath = "/env/lib/python3.6/site-packages"
+timeout = 3600
