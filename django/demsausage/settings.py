8eb45b0e5091df41c8c3456e90608b74d3fca096
Keith Moss <keithamoss@gmail.com>
2021-04-04 14:32:36 +0800

    Yahoo Auth doesn't exist any more, and remove the rest because we only use Google anyway

--- django/demsausage/settings.py
+++ django/demsausage/settings.py
@@ -122,9 +122,6 @@ MIDDLEWARE = [
 
 AUTHENTICATION_BACKENDS = (
     'social_core.backends.google.GoogleOAuth2',
-    'social_core.backends.twitter.TwitterOAuth',
-    'social_core.backends.yahoo.YahooOpenId',
-    'social_core.backends.facebook.FacebookOAuth2',
     'django.contrib.auth.backends.ModelBackend',
 )
 
