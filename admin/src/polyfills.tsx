d469f5b4d68b43540538dfa69ed05c59433ff062
Keith Moss <keithamoss@gmail.com>
2021-04-06 15:55:09 +0800

    [Admin] Delete old codebase

--- admin/src/polyfills.tsx
+++
@@ -1,8 +0,0 @@
-// Import any specific polyfills required for legacy browsers
-import "core-js/modules/es6.array.find"
-import "core-js/modules/es6.array.find-index"
-import "core-js/modules/es6.object.keys"
-import "core-js/modules/es6.promise"
-import "core-js/modules/es7.array.includes"
-import "core-js/modules/es7.object.entries"
-import "core-js/modules/es7.object.values"
