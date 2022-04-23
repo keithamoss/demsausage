7a3fd77ae8df40192e5a43ffc72fa3ea83efa8e4
Keith Moss <keithamoss@gmail.com>
2022-04-23 22:16:00 +0800

    Adding a script to load overseas polling place data

---
+++ scrapers/federal_2022/merge_overseas.py
@@ -0,0 +1,31 @@
+import copy
+import csv
+
+POLLING_PLACE_FILENAME = 'prdelms.gaz.statics.220423.09.00.02'
+OVERSEAS_POLLING_PLACE_FILENAME = 'overseas_polling_places_2022'
+MERGED_POLLING_PLACE_FILENAME = f'{POLLING_PLACE_FILENAME}_merged_overseas'
+
+# open both files
+with open(f'data/{POLLING_PLACE_FILENAME}.csv') as polling_places_australia, open(f'data/{OVERSEAS_POLLING_PLACE_FILENAME}.csv') as polling_places_overseas:
+    csv_polling_places_australia = csv.DictReader(polling_places_australia)
+    list_polling_places_australia = list(csv_polling_places_australia)
+    print(len(list_polling_places_australia))
+
+    csv_polling_places_overseas = csv.DictReader(polling_places_overseas)
+    list_polling_places_overseas = list(csv_polling_places_overseas)
+    print(len(list_polling_places_overseas))
+
+    polling_places_all = list_polling_places_australia + list_polling_places_overseas
+    print(len(polling_places_all))
+
+    print(csv_polling_places_overseas.fieldnames)
+
+    check_file_similarity = copy.deepcopy(csv_polling_places_overseas.fieldnames)
+    check_file_similarity.remove('booth_info')
+    if csv_polling_places_australia.fieldnames != check_file_similarity:
+        raise Exception('The two polling place files should be identical except for the booth_info field')
+
+    with open(f'data/{MERGED_POLLING_PLACE_FILENAME}.csv', 'w') as output_file:
+        dict_writer = csv.DictWriter(output_file, fieldnames=csv_polling_places_overseas.fieldnames)
+        dict_writer.writeheader()
+        dict_writer.writerows(polling_places_all)
