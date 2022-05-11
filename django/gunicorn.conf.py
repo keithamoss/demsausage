dfdebbf624a25c0d373adef6e72c84d4623192d4
Keith Moss <keithamoss@gmail.com>
2022-05-11 19:09:27 +0800

    Tweak Gunicorn and NGinx settings for resiliency

--- django/gunicorn.conf.py
+++ django/gunicorn.conf.py
@@ -8,10 +8,11 @@ def max_workers():
 
 
 bind = "0.0.0.0:" + environ.get("PORT", "8000")
-max_requests = 15000
+max_requests = 1000
+max_requests_jitter = 30
 worker_class = "gevent"
 workers = max_workers()
 forwarded_allow_ips = "*"
 loglevel = "info"
 pythonpath = "/env/lib/python3.6/site-packages"
-timeout = 3600
+timeout = 120  # This could maybe be 30s like the nginx-level conf, but would that break the RQ queue?
