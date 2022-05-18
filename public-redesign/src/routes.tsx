511571c5cb6243f8481ca52c1f5c501151946921
Keith Moss <keithamoss@gmail.com>
2022-05-18 20:04:42 +0800

    [Public Redesign] WIP

--- public-redesign/src/routes.tsx
+++ public-redesign/src/routes.tsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import DemoPage from "./pages/Demo";
 import FoobarPage from "./pages/Foobar";
+import ReactSpring from "./pages/ReactSpring";
+import SwipePage from "./pages/Swipe";
 
 const routes = () => {
   return (
     <Routes>
       <Route path="/" element={<DemoPage />}></Route>
       <Route path="/foobar" element={<FoobarPage />}></Route>
+      <Route path="/swipe" element={<SwipePage />}></Route>
+      <Route path="/react-spring" element={<ReactSpring />}></Route>
     </Routes>
   );
 };
