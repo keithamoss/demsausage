243c71e19f5cd9156ed0ab5340cae2ed308d0a91
Keith Moss <keithamoss@gmail.com>
2022-12-23 15:21:56 +0800

    OK, let's try this

--- prodbuild.sh
+++ prodbuild.sh
@@ -13,20 +13,21 @@ if [ "$CMD" = "frontend" ] || [ "$CMD" = "all" ]; then
     mkdir -p nginx-prod/build/
 
     # build the frontend assets (this takes quite a while due to minification)
-    docker-compose -f docker-compose-buildjs.yml run frontend_public
-    docker-compose -f docker-compose-buildjs.yml stop
+    # docker-compose -f docker-compose-buildjs.yml run frontend_public
+    # docker-compose -f docker-compose-buildjs.yml stop
     docker-compose -f docker-compose-buildjs.yml run frontend_public_redesign
     docker-compose -f docker-compose-buildjs.yml stop
-    docker-compose -f docker-compose-buildjs.yml run frontend_admin
-    docker-compose -f docker-compose-buildjs.yml stop
+    # docker-compose -f docker-compose-buildjs.yml run frontend_admin
+    # docker-compose -f docker-compose-buildjs.yml stop
 
     # build the django assets
-    docker-compose -f docker-compose-buildpy.yml build
-    docker-compose -f docker-compose-buildpy.yml run django
-    docker-compose -f docker-compose-buildpy.yml stop
+    # docker-compose -f docker-compose-buildpy.yml build
+    # docker-compose -f docker-compose-buildpy.yml run django
+    # docker-compose -f docker-compose-buildpy.yml stop
 
     # copy assets locally for build local production nginx image (local testing only)
-    cp build/frontend-public.tgz build/frontend-public-redesign.tgz build/frontend-admin.tgz build/django.tgz nginx-prod/build # this is horrible, fixme
+    # cp build/frontend-public.tgz build/frontend-public-redesign.tgz build/frontend-admin.tgz build/django.tgz nginx-prod/build # this is horrible, fixme
+    cp build/frontend-public-redesign.tgz nginx-prod/build # this is horrible, fixme
 
     # For local testing with docker-compose-prod.yml only
     # echo building prod nginx container
