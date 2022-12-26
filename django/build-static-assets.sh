d4f9dc0b3fbdcdc178d21c8c55f0d0563f313720
Keith Moss <keithamoss@gmail.com>
2022-12-26 13:56:15 +0800

    Try this

--- django/build-static-assets.sh
+++ django/build-static-assets.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+echo "Build..."
 docker-compose build django
+echo "Run..."
 docker-compose run django build
+echo "Tar..."
 tar czvf ../nginx-prod/build/django.tgz ./static/
+echo "Stop..."
 docker-compose stop
