f8d4b264d88639a8f5e13d46d023ef88e7578c88
Keith Moss <keithamoss@gmail.com>
2022-12-28 09:24:59 +0800

    MTW

--- build.sh
+++ build.sh
@@ -1,10 +1,42 @@
 #!/bin/bash
 
-pwd
-ls -l ./secrets
-echo "Building..."
-docker compose -f build.yml build django
-pwd
-ls -l ./secrets
-echo "Running..."
-docker compose -f build.yml run django
+CMD="$1"
+
+if [ x"$CMD" = x ]; then
+    echo "provide a command!"
+    exit 1
+fi
+
+if [ "$CMD" = "full-run" ]; then
+    # Build - Prep
+    rm -rf ./nginx-prod/build/
+    mkdir ./nginx-prod/build
+fi
+
+if [ "$CMD" = "frontend-only" ] || [ "$CMD" = "full-run" ]; then
+    # Public Frontend - Build
+    cd public && ./build.sh && cd ../
+
+    # Public Redesign - Build
+    cd public-redesign && ./build.sh && cd ../
+
+    # Admin - Build
+    cd admin && ./build.sh && cd ../
+
+    # Containers - Build Nginx
+    cd nginx-prod && docker build -t sausage2/nginx:latest . && cd ../
+fi
+
+if [ "$CMD" = "django-only" ] || [ "$CMD" = "full-run" ]; then
+    # Django - Build Static Assets
+    docker compose --file build.yml run django
+    
+    # Containers - Build Django
+    cd django && docker build -t sausage2/django:latest . && cd ../
+fi
+
+# Containers - Push to Docker Hub
+docker tag sausage2/nginx:latest keithmoss/sausage2-nginx:latest
+docker push keithmoss/sausage2-nginx:latest
+docker tag sausage2/django:latest keithmoss/sausage2-django:latest
+docker push keithmoss/sausage2-django:latest
--- public/build.sh
+++ public/build.sh
@@ -3,5 +3,7 @@
 rm -rf ./build
 mkdir -p ./build
 
+yarn run lint
+
 yarn run build
 tar czvf ../nginx-prod/build/public.tgz ./build/
