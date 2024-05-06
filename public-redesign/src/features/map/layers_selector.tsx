482196656c66e941e543e18b976fff227c5f62f7
Keith Moss <keithamoss@gmail.com>
2024-05-07 07:14:16 +0800

    We now show the name of the election if you're not viewing the current default live election

--- public-redesign/src/features/map/layers_selector.tsx
+++ public-redesign/src/features/map/layers_selector.tsx
@@ -1,7 +1,7 @@
 import CloseIcon from '@mui/icons-material/Close';
 import LayersIcon from '@mui/icons-material/Layers';
 import LiveTvIcon from '@mui/icons-material/LiveTv';
-import { Badge, ListItemButton, useTheme } from '@mui/material';
+import { Badge, Button, ListItemButton, useTheme } from '@mui/material';
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
@@ -11,14 +11,17 @@ import ListItem from '@mui/material/ListItem';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import ListItemText from '@mui/material/ListItemText';
 import ListSubheader from '@mui/material/ListSubheader';
+import { blueGrey, grey } from '@mui/material/colors';
 import { styled } from '@mui/material/styles';
 import { groupBy, sortBy } from 'lodash-es';
 import * as React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useCallback } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useAppSelector } from '../../app/hooks';
+import { getStringParamOrUndefined } from '../../app/routing/routingHelpers';
 import { Election } from '../../app/services/elections';
 import { mapaThemePrimaryPurple } from '../../app/ui/theme';
-import { getElectionVeryShortName, isElectionLive } from '../elections/electionHelpers';
+import { getDefaultElection, getElectionVeryShortName, isElectionLive } from '../elections/electionHelpers';
 import { selectActiveElections, selectAllElections } from '../elections/electionsSlice';
 
 interface Props {
@@ -39,6 +42,9 @@ const StyledLayersBadge = styled(Badge)(({ theme }) => ({
 
 const StyledIconButton = styled(IconButton)(() => ({
 	backgroundColor: 'white',
+	'&:hover': {
+		backgroundColor: grey[200],
+	},
 }));
 
 const StyledCloseIconButton = styled(IconButton)(() => ({
@@ -57,6 +63,9 @@ const StyledInteractableBoxFullHeight = styled(Box)(({ theme }) => ({
 export default function LayersSelector(props: Props) {
 	const { electionId } = props;
 
+	const theme = useTheme();
+
+	const params = useParams();
 	const navigate = useNavigate();
 
 	const [state, setState] = React.useState(false);
@@ -68,18 +77,23 @@ export default function LayersSelector(props: Props) {
 		new Date(e.election_day).getFullYear(),
 	);
 
-	const theme = useTheme();
+	const urlElectionName = getStringParamOrUndefined(params, 'election_name');
+	const isViewingHistoricalElection =
+		urlElectionName !== undefined && getDefaultElection(elections)?.name_url_safe !== urlElectionName;
 
-	const toggleDrawer = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
-		if (
-			event.type === 'keydown' &&
-			((event as React.KeyboardEvent).key === 'Tab' || (event as React.KeyboardEvent).key === 'Shift')
-		) {
-			return;
-		}
+	const toggleDrawer = useCallback(
+		(open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+			if (
+				event.type === 'keydown' &&
+				((event as React.KeyboardEvent).key === 'Tab' || (event as React.KeyboardEvent).key === 'Shift')
+			) {
+				return;
+			}
 
-		setState(open);
-	};
+			setState(open);
+		},
+		[],
+	);
 
 	const onClickElection = (election: Election) => () => navigate(`/${election.name_url_safe}`);
 
@@ -91,6 +105,21 @@ export default function LayersSelector(props: Props) {
 				className="layer-selector"
 				sx={{ marginTop: '46px' }}
 			>
+				{isViewingHistoricalElection === true && (
+					<Button
+						size="small"
+						disabled={true}
+						sx={{
+							backgroundColor: 'white',
+							color: `${blueGrey.A700} !important`,
+							left: '20px',
+							paddingRight: '25px',
+						}}
+					>
+						{elections.find((e) => e.name_url_safe === urlElectionName)?.name}
+					</Button>
+				)}
+
 				<StyledIconButton aria-label="layers" onClick={toggleDrawer(true)}>
 					<LayersIcon />
 				</StyledIconButton>
