21a90f281d2806aeef5aaa22515eaccdf5693bb3
Keith Moss <keithamoss@gmail.com>
2024-06-15 14:45:40 +0800

    Making builds work

--- public/docker-entrypoint.sh
+++ public/docker-entrypoint.sh
@@ -3,6 +3,10 @@
 command="$1"
 cd /app
 
+# Presence of the "packageManager" field indicates that the project is meant to be used with Corepack, a tool included by default with all official Node.js distributions starting from 16.9 and 14.19.
+# Corepack must currently be enabled by running corepack enable in your terminal. For more information, check out https://yarnpkg.com/corepack.
+corepack enable
+
 if [ ! -d ".yarn" ]; then
   rm -f .yarn*
 fi
