cc4ece9289996e0dc8846c00c598d0a784ee2883
Keith Moss <keithamoss@gmail.com>
2025-02-08 18:05:07 +0800

    Update docker-entrypoint.sh

--- admin-redesign/docker-entrypoint.sh
+++ admin-redesign/docker-entrypoint.sh
@@ -3,6 +3,13 @@
 command="$1"
 cd /app
 
+# Temporarily workaround a corepack bug
+# Ref: https://stackoverflow.com/a/79415251/7368493
+echo "Before: corepack version => $(corepack --version || echo 'not installed')"
+npm install -g corepack@latest
+echo "After : corepack version => $(corepack --version)"
+corepack enable
+
 corepack enable pnpm
 
 if [ ! -d node_modules ]; then
