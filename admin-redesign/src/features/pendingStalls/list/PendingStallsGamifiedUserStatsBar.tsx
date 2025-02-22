7a6924460cd99b0fdac9a5f6e4151612dd6cacf0
Keith Moss <keithamoss@gmail.com>
2025-02-22 22:40:16 +0800

    Remove the manual tooltip partial implementaton
    It wasn't handling close tooltips on click elsewhere

--- admin-redesign/src/features/pendingStalls/list/PendingStallsGamifiedUserStatsBar.tsx
+++ admin-redesign/src/features/pendingStalls/list/PendingStallsGamifiedUserStatsBar.tsx
@@ -1,5 +1,4 @@
 import { Avatar, Badge, Box, Fab } from '@mui/material';
-import { useState } from 'react';
 import type { ElectionPendingStallsGamifiedUserStats } from '../../../app/services/stalls';
 import { mapaThemePrimaryPurple } from '../../../app/ui/theme';
 
@@ -13,16 +12,6 @@ export default function PendingStallsGamifiedUserStatsBar(props: Props) {
 	const userStatsGrandTotal = stats.map((item) => item.total).reduce((sum, item) => sum + item);
 	const userStatsSingleUserTotal = stats.map((item) => item.total).reduce((max, item) => (item > max ? item : max));
 	const avatarFabDiameter = 36;
-
-	const [activeTooltipId, setActiveTooltipId] = useState<number | undefined>(undefined);
-
-	const onClickFab = (id: number) => () => {
-		if (activeTooltipId === id) {
-			setActiveTooltipId(undefined);
-		} else {
-			setActiveTooltipId(id);
-		}
-	};
 
 	return (
 		<Box sx={{ display: 'flex', alignItems: 'center', position: 'relative', mt: 1 }}>
@@ -44,29 +33,23 @@ export default function PendingStallsGamifiedUserStatsBar(props: Props) {
 					top: 0,
 				}}
 			>
-				{stats.map((item) => {
-					// console.log(item.id, 'is', tooltipsState.includes(item.id));
-					return (
-						// <Tooltip key={item.id} open={activeTooltipId === item.id} title={`${item.name}: ${item.total}`}>
-						<Fab
-							key={item.id}
-							color="primary"
-							sx={{
-								position: 'absolute',
-								width: avatarFabDiameter,
-								height: avatarFabDiameter,
-								// Take 3% off to (roughly) accommodate the width of 36px
-								left: `${(item.total / userStatsGrandTotal) * 100 - 3}%`,
-							}}
-							onClick={onClickFab(item.id)}
-						>
-							<Badge badgeContent={item.total} max={999} color="primary">
-								<Avatar src={item.image_url} alt={item.initial} sx={{ width: 24, height: 24 }} />
-							</Badge>
-						</Fab>
-						// </Tooltip>
-					);
-				})}
+				{stats.map((item) => (
+					<Fab
+						key={item.id}
+						color="primary"
+						sx={{
+							position: 'absolute',
+							width: avatarFabDiameter,
+							height: avatarFabDiameter,
+							// Take 3% off to (roughly) accommodate the width of 36px
+							left: `${(item.total / userStatsGrandTotal) * 100 - 3}%`,
+						}}
+					>
+						<Badge badgeContent={item.total} max={999} color="primary">
+							<Avatar src={item.image_url} alt={item.initial} sx={{ width: 24, height: 24 }} />
+						</Badge>
+					</Fab>
+				))}
 			</Box>
 		</Box>
 	);
