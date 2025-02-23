6648f377b6d5a0d6d31cf764f0347f207145adaf
Keith Moss <keithamoss@gmail.com>
2025-02-23 08:16:58 +0800

    Fixing the pendingStallsPollingPlaceHelpers "Cannot find module" issue

--- admin-redesign/src/features/pendingStalls/pollingplace/PendingStallsPollingPlace.tsx
+++ admin-redesign/src/features/pendingStalls/pollingplace/PendingStallsPollingPlace.tsx
@@ -15,6 +15,8 @@ import {
 	type PendingStall,
 	type PollingPlaceWithPendingStall,
 	StallApprovalType,
+	type StallFoodOptions,
+	type StallNonFoodOptions,
 	useApproveStallMutation,
 	useDeclineStallMutation,
 	useGetPendingStallsQuery,
@@ -24,7 +26,6 @@ import PollingPlaceNomsEditorForm from '../../pollingPlaces/PollingPlaceNomsEdit
 import { getPollingPlaceSummaryCardForHeading } from '../../pollingPlaces/pollingPlaceHelpers';
 import { mergeStallWithPollingPlaceFormNomsAndUpdateForm } from '../../pollingPlaces/pollingPlaceNomsEditorFormHelpers';
 import type { IPollingPlaceStallModifiableProps } from '../../pollingPlaces/pollingPlacesInterfaces';
-import { isStallMergedWithPollingPlace } from '../pendingStallsPollingPlaceHelpers';
 import PendingStallPollingPlaceHistory from './PendingStallPollingPlaceHistory';
 import PendingStallPollingPlaceSubmissions from './PendingStallPollingPlaceSubmissions';
 import PendingStallsPollingPlaceAndStallsSummary from './PendingStallsPollingPlaceAndStallsSummary';
@@ -48,6 +49,30 @@ const PageWrapper = styled('div')(({ theme }) => ({
 	// See above
 	// paddingBottom: theme.spacing(8),
 }));
+
+const isStallMergedWithPollingPlace = (stall: PendingStall, pollingPlaceStall: IPollingPlaceStallModifiableProps) => {
+	// This is a quick and dirty merge check:
+	// If the noms on the stall match the polling place, we'll consider the submission merged,
+
+	let key: keyof (StallFoodOptions & StallNonFoodOptions);
+
+	for (key in stall.noms) {
+		const value = stall.noms[key];
+
+		if (key === 'free_text') {
+			if (pollingPlaceStall.noms[key] === undefined || pollingPlaceStall.noms[key] === '') {
+				return false;
+			}
+		} else {
+			if (pollingPlaceStall.noms[key] === undefined || pollingPlaceStall.noms[key] !== true) {
+				return false;
+			}
+		}
+	}
+
+	// All of the noms have been applied, so we're good!
+	return true;
+};
 
 function EntrypointLayer1() {
 	const urlPollingPlaceId = getIntegerParamOrUndefined(useParams(), 'polling_place_id');
