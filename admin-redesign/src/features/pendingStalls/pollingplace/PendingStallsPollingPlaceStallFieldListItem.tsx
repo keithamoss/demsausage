cb3043b679894fe67bf78c318ca5be88a5b2603b
Keith Moss <keithamoss@gmail.com>
2025-03-02 11:25:31 +0800

    Free text noms are now diff'd separately to regular selectable noms

--- admin-redesign/src/features/pendingStalls/pollingplace/PendingStallsPollingPlaceStallFieldListItem.tsx
+++ admin-redesign/src/features/pendingStalls/pollingplace/PendingStallsPollingPlaceStallFieldListItem.tsx
@@ -10,6 +10,7 @@ import {
 } from '@mui/icons-material';
 import { IconButton, ListItem, ListItemIcon, ListItemText, styled } from '@mui/material';
 import { useNotifications } from '@toolpad/core';
+import { isEqual } from 'lodash-es';
 import React, { useCallback } from 'react';
 import type { PendingStall, StallFoodOptions } from '../../../app/services/stalls';
 import { mapaThemePrimaryGrey } from '../../../app/ui/theme';
@@ -83,6 +84,16 @@ const getFieldValueForDisplay = (fieldName: string, fieldValue: string | number 
 		return undefined;
 	}
 
+	// Support translating noms to text for the diff'd view (the current view already gets translated by getFieldValue())
+	if (fieldName === 'noms' && typeof fieldValue !== 'string') {
+		return getNomsDescriptiveTextWithoutFreeText(fieldValue as StallFoodOptions);
+	}
+
+	// Support translating free_text noms to text for the diff'd view (the current view already gets translated by getFieldValue())
+	if (fieldName === 'noms.free_text' && typeof fieldValue !== 'string') {
+		return (fieldValue as StallFoodOptions).free_text;
+	}
+
 	if (fieldName === 'website') {
 		return typeof fieldValue === 'string' && isStallWebsiteValid(fieldValue) === true ? (
 			<a href={fieldValue} target="blank" style={{ color: 'blue' }}>
@@ -98,6 +109,32 @@ const getFieldValueForDisplay = (fieldName: string, fieldValue: string | number 
 	return `${fieldValue}` || <em>Not provided</em>;
 };
 
+const getFieldDiff = (
+	fieldName: 'noms' | 'name' | 'description' | 'opening_hours' | 'website' | 'noms.free_text',
+	stall: PendingStall,
+) => {
+	const fieldDiff = stall.diff?.find((d) => d.field === (fieldName === 'noms.free_text' ? 'noms' : fieldName));
+
+	if (fieldDiff === undefined) {
+		return undefined;
+	}
+
+	if (fieldName === 'noms') {
+		const { free_text: free_text_old, ...restOld } = fieldDiff.old as StallFoodOptions;
+		const { free_text: free_text_new, ...restNew } = fieldDiff.new as StallFoodOptions;
+
+		return isEqual(restOld, restNew) === true ? undefined : fieldDiff;
+	}
+
+	if (fieldName === 'noms.free_text') {
+		return (fieldDiff.old as StallFoodOptions).free_text === (fieldDiff.new as StallFoodOptions).free_text
+			? undefined
+			: fieldDiff;
+	}
+
+	return fieldDiff;
+};
+
 interface Props {
 	fieldName: 'noms' | 'name' | 'description' | 'opening_hours' | 'website' | 'noms.free_text';
 	fieldLabel: string;
@@ -111,7 +148,7 @@ export default function PendingStallsPollingPlaceStallFieldListItem(props: Props
 
 	const fieldValue = getFieldValue(fieldName, stall);
 
-	const fieldDiff = stall.diff?.find((d) => d.field === fieldName);
+	const fieldDiff = getFieldDiff(fieldName, stall);
 
 	const isFieldCopyable = fieldName !== 'noms' && fieldValue !== '';
 
