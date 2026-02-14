a004e525922e485c25f21e5dd1b62b3f36262030
Keith Moss <keith.moss@dpc.wa.gov.au>
2026-02-14 18:55:40 +0800

    Improving cron logging and doco

--- django/demsausage/app/sausage/cron/cron.sh
+++ django/demsausage/app/sausage/cron/cron.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] Starting cron job"
 /usr/local/bin/python /app/demsausage/app/sausage/cron/cron.py
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] Cron job completed"
